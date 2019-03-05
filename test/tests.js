39c4874d0c14ed77d1ddfb130c2eb8cf67d656f1
emily baier <emilyrbaier@gmail.com>
2019-03-05 10:38:56 -0800

    passing html template test

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,40 @@
 import './html-equal.js';
+
+const test = QUnit.test;
+
+function makeMovieTemplate(movie) {
+    const html = `
+    <li>
+    <h2>Star Wars</h2>
+    <img src="https://image.tmdb.org/t/p/w92/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg">
+    <p>Release Date: 1977-05-25</p>
+    </li>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+test('template will be the same as static html', assert => {
+    //arrange
+    const movie = {
+        'id': 11,
+        'title': 'Star Wars',
+        'poster_path': '/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg',
+        'release_date': '1977-05-25'
+    };
+
+    //act
+    const result = makeMovieTemplate(movie);
+
+    //assert
+    assert.htmlEqual(result, /*html*/ `
+    <li>
+    <h2>Star Wars</h2>
+    <img src="https://image.tmdb.org/t/p/w92/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg">
+    <p>Release Date: 1977-05-25</p>
+    </li>
+`);
+
+});
