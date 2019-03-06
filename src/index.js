ec6145246fb66159703b367c3e9773ce1d6d82a5
emily baier <emilyrbaier@gmail.com>
2019-03-05 16:22:36 -0800

    fetching movie data from the api now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import loadMovies from './load-movies.js';
-import movies from '../data/fake-ass-movies.js';
 import './search-component.js';
 import { readOptions } from './hash-query.js';
 import { updateSearchTerm } from './search-component.js';
+import makeSearchMovieUrl from './make-search-movie-url.js';
 
-loadMovies(movies);
+
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
     const queryOptions = readOptions(query);
     console.log(queryOptions);
     updateSearchTerm(queryOptions.searchTerm);
+
+    const url = makeSearchMovieUrl(queryOptions);
+    fetch(url) 
+        .then(response => response.json()) 
+        .then(results => {
+            loadMovies(results.results);
+        });
 });
+
--- src/load-movies.js
+++ src/load-movies.js
@@ -2,8 +2,15 @@ import makeMovieTemplate from './movie-template.js';
 const movieList = document.getElementById('movie-list');
 
 export default function loadMovies(movies) {
+    clearImages();
     movies.forEach(movie => {
         const dom = makeMovieTemplate(movie);
         movieList.appendChild(dom);
     });
+}
+
+function clearImages() {
+    while(movieList.children.length > 0) {
+        movieList.lastElementChild.remove();
+    }
 }
